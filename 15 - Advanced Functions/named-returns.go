64b97b65ec2036a9c0c386683689dd3dc4e0199f
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2021-04-19 18:11:40 -0300

    feat: functions - named returns

---
+++ 15 - Advanced Functions/named-returns.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func mathCalcs(n1, n2 int8) (sum int8, sub int8) {
+	sum = n1 + n2
+	sub = n1 - n2
+
+	return
+}
+
+func main() {
+	fmt.Println("Named returns")
+
+	sum, sub := mathCalcs(6, 2)
+
+	fmt.Println("SUM", sum)
+	fmt.Println("SUB", sub)
+}
